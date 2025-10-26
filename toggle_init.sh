9f24f77ee3e33314fd8dc265efeac822d33fb1f5
philippkastws <philipp@kast.ws>
2025-10-26 19:55:23 +0100

    Rename init.lua

--- toggle_init.sh
+++ toggle_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f "init.vim" ]; then
-  mv init.vim init.vim.NO
-  mv init.lua.NO init.lua
+  mv init.vim init.NO.vim
+  mv init.NO.lua init.lua
 else
-  mv init.vim.NO init.vim
-  mv init.lua init.lua.NO
+  mv init.NO.vim init.vim
+  mv init.lua init.NO.lua
 fi
