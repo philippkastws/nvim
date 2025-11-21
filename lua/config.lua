1e82df2a6be8acb3298cfdd492854157fb72b5f8
philippkastws <philipp@kast.ws>
2025-11-21 22:57:48 +0100

    Move some settings from kick start

--- lua/config.lua
+++ lua/config.lua
@@ -10,3 +10,26 @@ vim.opt.spelloptions = "camel"
 vim.opt.spellcapcheck = ""
 vim.o.spell = false
 
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
