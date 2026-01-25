2afcab7980a3f9a99a7e98a969b8cda8c34d24d3
philippkastws <philipp@kast.ws>
2026-01-25 21:17:50 +0100

    Fix lua lint

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,6 +1,8 @@
+---@diagnostic disable: undefined-global
 local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end-- Check if we need to reload the file when it changed
+end
+-- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
   group = augroup("checktime"),
   callback = function()
