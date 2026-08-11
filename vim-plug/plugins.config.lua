13492059f5a99796d785da6e29be6e1cac5e5c35
philippkastws <philipp@kast.ws>
2026-08-11 21:14:04 +0200

    Overwrite red for everforest

--- vim-plug/plugins.config.lua
+++ vim-plug/plugins.config.lua
@@ -2369,8 +2369,8 @@ end)
 
 pcall(function()
   require("everforest").setup({
-    -- colours_override = function (palette)
-    --   palette.red = "#30baac"
-    -- end
+    colours_override = function (palette)
+      palette.red = "#646fb8"
+    end
   })
 end)
